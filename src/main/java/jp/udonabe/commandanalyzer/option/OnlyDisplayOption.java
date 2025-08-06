11e7ce58845faad3f77f49cffe75ab95c0659b73
Yu WATANABE <yu_w_main@yahoo.co.jp>
2025-08-06 09:52:24 +0900

    [add] OnlyDisplayOptionの追加 表示名のみで、接頭辞が無いオプションであるOnlyDisplayOptionを追加しました。

---
+++ src/main/java/jp/udonabe/commandanalyzer/option/OnlyDisplayOption.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025 Command-Analyzer Contributors.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+package jp.udonabe.commandanalyzer.option;
+
+import lombok.NonNull;
+
+/**
+ * 表示のみで、接頭辞もなく、値も受け取らないオプション。
+ */
+public class OnlyDisplayOption extends Option {
+    /**
+     * コンストラクタ。
+     * argTypeは自動的にNONEになります。
+     * @param name 管理名。
+     * @param displayName 表示名。
+     * @param required 必須かどうか。
+     */
+    public OnlyDisplayOption(@NonNull String name, String displayName, boolean required) {
+        super("", name, displayName, ArgType.NONE, required);
+    }
+}
