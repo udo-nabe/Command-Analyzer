60b8c6f1c137cad8f0825143ecda1c505c8977ca
udo-nabe <yu_w_main@yahoo.co.jp>
2025-09-01 20:44:41 +0900

    WIP: [remove] Option系クラスの削除
    仕様変更に伴い、Optionのサブクラスを削除し、代わりにOptionに複数フィールドを追加し、さらにレコード化しました。

--- src/main/java/io/github/udonabe/commandanalyzer/option/Option.java
+++ src/main/java/io/github/udonabe/commandanalyzer/option/Option.java
@@ -8,89 +8,61 @@
 
 package io.github.udonabe.commandanalyzer.option;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NonNull;
-import lombok.ToString;
+
+import java.util.List;
 
 /**
  * コマンドのオプションを表すクラス。
- * 一般的には、このクラスを直接インスタンス化することはせず、
- * 子クラスをインスタンス化した方が意図が分かりやすくなります。
- * ただし、独自の接頭辞を使う場合は、このクラスをインスタンス化するか、
- * このクラスを拡張してください。
  */
-
-@Getter
-@EqualsAndHashCode
-@ToString
-public sealed class Option permits ArgumentOption, LongOption, ShortOption, SlashOption, SubCommandOption {
-    /**
-     * オプションの接頭辞。空にすることもできます。
-     * 注意: このクラスを直接インスタンス化しても、子クラス特有の機能(ショートオプションをまとめられるなど)は付与されません。
-     */
-    private final String prefix;
-    /**
-     * オプションの表示名。
-     * 表示名は空にすることができます。
-     * 空にした場合、ArgTypeをNONEに設定することはできません。
-     */
-    private final String displayName;
-    /**
-     * 引数の種別。詳細は{@link ArgType}をご覧ください。
-     */
-    private final ArgType argType;
+public record Option(Kind kind,
+                     List<String> names,
+                     ArgType type,
+                     boolean required,
+                     String exclusiveGroup,
+                     String description,
+                     String managementName) {
 
     /**
-     * 標準のコンストラクタ。
-     * @param prefix 接頭辞。({@link #prefix})
-     * @param argType 引数の種別。({@link #argType})
+     * 文字列が自身のプレフィックス+内容(namesの要素一つ一つ)と等価か調べる。
+     * @param in 比較対象。
+     * @return 調べた結果。
      */
-    public Option(@NonNull String prefix, String displayName, @NonNull ArgType argType) {
-        //引数のチェック
-        if (displayName == null || displayName.isEmpty()) {
-            if (!(this instanceof SubCommandOption || this instanceof ArgumentOption)) {
-                throw new IllegalArgumentException("Argument displayName cannot be null or empty");
-            }
-        }
-        if (displayName != null) {
-            if (!(prefix.equals("-") || prefix.equals("--") || prefix.equals("/") || prefix.isEmpty())) {
-                throw new IllegalArgumentException("Argument prefix must be \"-\" or \"--\" or \"/\" or empty.");
-            }
-            if (displayName.startsWith("-") || displayName.startsWith("--") || displayName.startsWith("/")) {
-                throw new IllegalArgumentException("Argument displayName cannot be \"-\" or \"--\" or \"/\"");
-            }
-        }
-
-        this.prefix = prefix;
-        this.displayName = displayName;
-        this.argType = argType;
+    public boolean matches(String in) {
+        return names.stream().anyMatch(n -> (kind.getPrefix() + n).equals(in));
     }
 
-    /**
-     * 引数の種別。
-     */
+    public static Option shortOption(String name, ArgType type, boolean required, String description, String managementName) {
+        return new Option(Kind.SHORT_OPTION, List.of(name), type, required, null, description, managementName);
+    }
+
+    public static Option longOption(String name, ArgType type, boolean required, String description, String managementName) {
+        return new Option(Kind.LONG_OPTION, List.of(name), type, required, null, description, managementName);
+    }
+
+    public static Option slashOption(String name, ArgType type, boolean required, String description, String managementName) {
+        return new Option(Kind.SLASH_OPTION, List.of(name), type, required, null, description, managementName);
+    }
+
+    public enum Kind {
+        SHORT_OPTION("-"),
+        LONG_OPTION("--"),
+        SLASH_OPTION("/"),
+        SUBCOMMAND(""),
+        ARGUMENT("");
+        @Getter
+        private final String prefix;
+
+        Kind(String prefix) {
+            this.prefix = prefix;
+        }
+    }
+
     public enum ArgType {
-        /**
-         * このオプションは、整数を引数として受け取るということを意味します。
-         */
-        INTEGER,
-        /**
-         * このオプションは、真偽値を引数として受け取るということを意味します。
-         * @deprecated 代わりに、{@link #NONE}を使用することを推奨します。
-         */
-        @Deprecated BOOLEAN,
-        /**
-         * このオプションは、倍精度浮動小数点数を引数として受け取るということを意味します。
-         */
-        DOUBLE,
-        /**
-         * このオプションは、文字列を引数として受け取るということを意味します。
-         */
-        STRING,
-        /**
-         * このオプションは、引数としては何も受け取らず、存在するかどうか自体が真偽値として受け取られます。
-         */
         NONE,
+        STRING,
+        INTEGER,
+        DOUBLE,
+        BOOLEAN,
     }
 }
